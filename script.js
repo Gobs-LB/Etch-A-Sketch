380b745426682fe7b18cff63cfff501ef8781d96
LucaBS <lucabs95@hotmail.com>
2020-06-13 18:37:38 -0300

    Basic CSS and Row function

---
+++ script.js
@@ -0,0 +1,13 @@
+function createRow(){
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("rowFlex");
+    
+    let mainBox = document.getElementById("mainBox");
+    mainBox.appendChild(newDiv);
+}
+
+
+
+for(i = 1; i<=16; i++){
+
+}
