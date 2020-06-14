deb0f8df91041acb3509b9d85a0b998fb8341be5
LucaBS <lucabs95@hotmail.com>
2020-06-14 13:11:46 -0300

    ChangeSize

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let gridSize = 16;
 
 function createRow(){
     let newDiv = document.createElement("div");
@@ -7,12 +8,16 @@ function createRow(){
     mainGrid.appendChild(newDiv);
 }
 
-for(let i = 1; i <= 16; i++){
-    createRow();
+function createAllRows(gridSize){
+    for(let i = 1; i <= gridSize; i++){
+        createRow();
+    }
 }
 
+createAllRows(gridSize);
+
 function createSquare(parentDiv){
-    for(let i = 1; i <= 16; i++){
+    for(let i = 1; i <= gridSize; i++){
         let newSquare = document.createElement("div");
         newSquare.classList.add("squares");
 
@@ -20,11 +25,46 @@ function createSquare(parentDiv){
     }
 }
 
-const rowList = Array.from(document.querySelectorAll(".rowFlex"));
-rowList.forEach(parentDiv => createSquare(parentDiv));
+function createAllSquares(gridSize){
+    const rowList = Array.from(document.querySelectorAll(".rowFlex"));
+    rowList.forEach(parentDiv => createSquare(parentDiv));
+}
 
-const squareList = Array.from(document.querySelectorAll(".squares"));
-squareList.forEach(square => square.addEventListener("mouseover", function(){
-    square.classList.add("blacked")
+createAllSquares(gridSize);
+
+function triggerHover(){
+    const squareList = Array.from(document.querySelectorAll(".squares"));
+    squareList.forEach(square => square.addEventListener("mouseover", function(){
+        square.classList.add("blacked")
+        }
+    ));
+}
+
+triggerHover();
+
+function enableRestartBtn(){
+    const restart = document.getElementById("restartBtn");
+    const squareList = Array.from(document.querySelectorAll(".squares"));
+    restart.addEventListener("click", function(){
+        squareList.forEach(square => square.classList.remove("blacked"))
+    });
+}
+
+enableRestartBtn();
+
+const changeSize = document.getElementById("sizeBtn");
+changeSize.addEventListener("click", function(){
+    gridSize = parseInt(prompt("Input new size:"));
+    if (isNaN(gridSize)){
+        return alert("That's not a number!");
     }
-));
+    else{
+        let mainGrid = document.getElementById("mainBox");
+        mainGrid.innerHTML = "";
+
+        createAllRows(gridSize);
+        createAllSquares(gridSize);
+        triggerHover();
+        enableRestartBtn();
+    }
+});
